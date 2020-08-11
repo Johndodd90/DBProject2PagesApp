e5ba59a299b332564cdccd27487a6828fb8a45e2
John Dodd <johndodd90@gmail.com>
2020-08-11 16:09:52 -0500

    Proc file added and Allowed hosts updated

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(56dbxxc))=o^4u4x)hs8xo0owr$4(l3z^!m^v@x$y-1^&6@o)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
